7bc7973f6ccc937e07fc6e1e36c851e0d146226b
krenwi <100181516+krenwi@users.noreply.github.com>
2022-08-24 15:40:19 +1200

    step 16

---
+++ src/Components/SearchBar/SearchBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { SearchBar } from "SearchBar.css"
+
+export class SearchBar extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    <div className="SearchBar">
+      <input placeholder="Enter A Song, Album, or Artist" />
+      <button className="SearchButton">SEARCH</button>
+    </div>
+  }
+}

09698fdf94a585664311fd361d67b4943a47dd0d
krenwi <100181516+krenwi@users.noreply.github.com>
2022-08-24 16:31:31 +1200

    step 21 errors resolved

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./SearchResults.css"
 
-export class SearchBar extends React.Component {
+export class SearchResults extends React.Component {
   render() {
     return (
       <div className="SearchResults">
       <h2>Results</h2>
-      <!-- Add a TrackList component -->
+      {/* <TrackList /> */}
     </div>
     )
   }

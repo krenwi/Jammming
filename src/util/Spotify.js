3afa0fad80dee2b8dc1a1dcdc4464945a02ec292
krenwi <100181516+krenwi@users.noreply.github.com>
2023-02-03 14:52:14 +1300

    step 80

---
+++ src/util/Spotify.js
@@ -0,0 +1,24 @@
+let accessToken
+
+const Spotify = {
+  getAccessToken() {
+    if(accessToken == true) {
+      return accessToken
+    }
+    //check url to see if it is obtained
+    const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/)
+    const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/)
+
+    if (accessTokenMatch && expiresInMatch) {
+      accessToken = accessTokenMatch[1]
+      const expiresIn = Number(expiresInMatch[1])
+
+      window.setTimeout(() => accessToken = '', expiresIn * 1000)
+      window.history.pushState('Access Token', null, '/')
+      return accessToken
+    }
+  }
+}
+
+
+export default Spotify
